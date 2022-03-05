f3fad0b7ebb915fe1efe4f07d490d133deaac8ab
denis listiadi <denislistiadi24@gmail.com>
2022-03-05 22:18:08 +0700

    Results Data

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,22 +1,44 @@
-import React from 'react'
-import { battle } from '../utils/api'
+import React from "react";
+import { battle } from "../utils/api";
 
 export default class Results extends React.Component {
-componentDidMount() {
-    const { playerOne, playerTwo } = this.props
+    constructor(props) {
+        super(props)
+
+        this.state ={
+            winner: null,
+            loser: null,
+            error: null,
+            loading: true
+        }
+    }
+  
+  componentDidMount() {
+    const { playerOne, playerTwo } = this.props;
 
     battle([playerOne, playerTwo])
-        .then((players) => {
-            console.log('data: ', players)
+    .then((players) => {
+      this.setState({
+        winner: players[0],
+        loser: players[1],
+        error: null,
+        loading: false
+      })
+    })
+    .catch(({message}) => {
+        this.setState({
+            error: message,
+            loading: false
         })
-}
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                Results
-                <pre>{JSON.stringify(this.props, null, 2)}</pre>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        Results
+        <pre>{JSON.stringify(this.props, null, 2)}</pre>
+      </div>
+    );
+  }
 }
