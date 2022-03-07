e450947b3cbd55b606bbe85293fdbc12e5cc67a3
denis listiadi <denislistiadi24@gmail.com>
2022-03-07 11:01:22 +0700

    Theme Provider

--- app/index.js
+++ app/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Battle from './components/Battle';
-import Popular from './components/Popular';
+import React from "react";
+import ReactDOM from "react-dom";
+import Battle from "./components/Battle";
+import Popular from "./components/Popular";
+import { ThemeProvider } from "./contexts/theme";
 import "./index.css";
 
-
 class App extends React.Component {
-    render() {
-        return (
-            <div className='container'>
-                <Battle />
-            </div>
-        )
+    constructor() {
+        super(props);
+
+        this.state = {
+            theme: "light",
+            toggleTheme: () => {
+                this.setState(({ theme }) => ({
+                    theme: theme === "light" ? "dark" : "light"
+                }))
+            }
+        }
     }
+  render() {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Battle />
+        </div>
+      </ThemeProvider>
+    );
+  }
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById("app"));
