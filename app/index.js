05f1ceec1be4fda01b15d6992e6c78e18da4e2c0
denis listiadi <denislistiadi24@gmail.com>
2022-03-07 15:24:34 +0700

    404 NotFound

--- app/index.js
+++ app/index.js
@@ -7,6 +7,7 @@ import { ThemeProvider } from "./contexts/theme";
 import "./index.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Results from "./components/Results";
+import { Switch } from "react-router-dom";
 
 class App extends React.Component {
   constructor(props) {
@@ -28,9 +29,12 @@ class App extends React.Component {
           <div className={this.state.theme}>
             <div className="container">
               <Nav />
-              <Route exact path='/' component={Popular} />
-              <Route exact path='/battle' component={Battle} />
-              <Route path='/battle/results' component={Results} />
+              <Switch>
+                <Route exact path="/" component={Popular} />
+                <Route exact path="/battle" component={Battle} />
+                <Route path="/battle/results" component={Results} />
+                <Route render={() => <h1>404 | Not Found</h1>} />
+              </Switch>
             </div>
           </div>
         </ThemeProvider>
